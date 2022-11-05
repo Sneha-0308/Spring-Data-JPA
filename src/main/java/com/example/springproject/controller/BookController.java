086b4c4ccbe8607549108f45c53f2a568d1e9f21
Sneha-0308 <mathadawar01@gmail.com>
2022-11-05 07:38:04 +0530

    Add status code

--- src/main/java/com/example/springproject/controller/BookController.java
+++ src/main/java/com/example/springproject/controller/BookController.java
@@ -3,10 +3,13 @@ package com.example.springproject.controller;
 import com.example.springproject.Service.BookService;
 import com.example.springproject.models.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 //@Controller
 /**when you use restcontroller as n=annotation then no need to mention the responsebody annotation**/
@@ -45,31 +48,53 @@ public class BookController {
     @Autowired
     BookService bookService;   //Could not autowire. No beans of 'BookService' type found. this error occurs when you have not annotated the service class as @Service
     @GetMapping("/books")
-    public List<Book> getAllBooks(){
-        return this.bookService.getAllBook();
-
+    public ResponseEntity<List<Book>> getAllBooks(){
+        List<Book> list=bookService.getAllBook();
+        if(list.size()<=0){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(list));
     }
 
     @GetMapping("/books/{id}")
-    public Book getBook(@PathVariable("id") int id){
-    return bookService.getBookById(id);
-
+    public ResponseEntity<Book> getBook(@PathVariable("id") int id){
+    Book book= bookService.getBookById(id);
+    if(book==null)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    return ResponseEntity.of(Optional.of(book));
     }
 
     @PostMapping("/books")
-    public Book addBook(@RequestBody Book book){
-        Book b=bookService.addBook(book);
-        return b;
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+        Book b=null;
+        try{
+            b=bookService.addBook(book);
+            return ResponseEntity.of(Optional.of(b));
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
-    @DeleteMapping("/books/{bookid}")
-    public void deleteBook(@PathVariable("bookid") int id){
-        bookService.deleteBook(id);
+    @DeleteMapping("/books/{bookid}")  //ResponseEntity<void> we can use instead of question mark
+    public ResponseEntity<?> deleteBook(@PathVariable("bookid") int id){
+        try{
+            bookService.deleteBook(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PutMapping("/books/{id}")
-    public Book updateBook(@RequestBody Book book,@PathVariable("id") int id){
-        bookService.updateBook(book,id);
-        return book;
+    public ResponseEntity<Book> updateBook(@RequestBody Book book,@PathVariable("id") int id){
+        try{
+            bookService.updateBook(book,id);
+            return ResponseEntity.ok().body(book);
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
